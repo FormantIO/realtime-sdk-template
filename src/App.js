cae6f2a57627669bd9bd91a6ab375fba125f96e5
Matthew Turnshek <matthewturnshek@gmail.com>
2021-07-18 23:50:45 -0400

    update template

--- src/App.js
+++ src/App.js
@@ -1,112 +1,101 @@
 import React, { Component} from 'react';
 import './App.css';
-import { RtcClient, Credentials, RtcSignalingClient, SignalingPromiseClient, AuthClient } from 'teleop-client';
-import { delay } from "./utils";
+import { RtcClient, SignalingPromiseClient } from '@formant/realtime-sdk';
 
+
+const formantApiUrl = "https://api-dev.formant.io";
+const decoder = new TextDecoder('utf-8');
+
+// This app is meant to run as a custom web view in Formant
+// with url e.g. http://localhost:3000/?auth={auth}&device={device_id}
 class App extends Component {
   constructor() {
     super()
-    const url = window.location.href;
-    this.userId = url.searchParams.get("deviceId");
-    this.deviceId = url.searchParams.get("deviceId");
-    this.token = url.searchParams.get("token");
-    // var c = url.searchParams.get("c");
-    // this.deviceId = 
+    this.deviceId = new URLSearchParams(window.location.search).get("device");
     this.dataChannel = undefined;
     this.canvas = undefined;
   }
 
-  async componentDidMount() {  
+  async componentDidMount() {
+    // Create an instance of the real-time communication client
+    const rtcClient = new RtcClient({
+      signalingClient: new SignalingPromiseClient(formantApiUrl, null, null),
+      getToken: () => (new URLSearchParams(window.location.search)).get("auth"),
+      receive: (peerId, message) => this.receiveRtcMessage(peerId, message),
+    });
+
+    // while (!rtcClient.isReady()) {
+    //   console.log("Waiting for RTC client to initialize...")
+    //   await delay(100);
+    // }
+    await delay(500); // TODO: update to latest realtime-sdk version and uncomment
+
+    // Each online device and user has a peer in the system
     const peers = await rtcClient.getPeers()
     console.log(peers);
 
+    // Find the device peer corresponding to the device's ID
     const devicePeer = peers.find(_ => _.deviceId !== undefined)
     if (!devicePeer) {
+      // If the device is offline, we won't be able to find its peer.
       console.log("Failed to find device peer.")
       return
     }
 
+    // We can connect our real-time communication client to device peers by their ID
     const devicePeerId = devicePeer.id;
+    await rtcClient.connect(devicePeerId)
 
-    await delay(1000);
-
-    const sessionId = await rtcClient.connect(devicePeerId)
-    console.log(sessionId);
-
+    // WebRTC requires a signaling phase when forming a new connection.
+    // Wait for the signaling process to complete...
     while (rtcClient.getConnectionStatus(devicePeerId) !== "connected") {
         await delay(100);
-        console.log("waiting for connection")
+        console.log("Waiting for connection ...")
     }
 
-    const decoder = new TextDecoder('utf-8')
-
+    // Create a custom data channel to the device peer with a name, settings, and handlers.
+    // The device-side application can send and receive messages
+    // on this channel using the agent API.
     rtcClient.createCustomDataChannel(
-      devicePeerId,
-      "test-sdk",
-      {
-        ordered: false,
-        maxRetransmits: 0
-      },
-      true,
+      devicePeerId, // device peer to open the channel with
+      "example-unreliable-channel", // channel name
+      { ordered: false, maxRetransmits: 0}, // channel settings
+      true, // use binary data format
       (_, channel) => {
         this.dataChannel = channel;
         channel.onopen = () => {
-          console.log("channel opened")
+          console.log("Channel opened.")
         }
-        channel.onmessage = (event) => {
-          console.log("rcv")
-          const encoded = decoder.decode(event.data);
-          const d = JSON.parse(encoded);
-          if (!this.canvas) {
-            return
-          }
-          this.canvas.width = 10 * d.width;
-          this.canvas.height = 10 * d.height;
-          const ctx = this.canvas.getContext("2d");
-
-          for (let i=0; i < d.data.length; i++) {
-            const width = 10 * (i % 60)
-            const height = 10 * Math.floor((i / 60.0))
-            ctx.shadowBlur = d.data[i];
-            ctx.strokeStyle='white';
-            ctx.shadowColor='blue';
-
-            ctx.fillStyle = `rgba(255, 255, 255, ${d.data[i]/100.0})`;
-            ctx.fillRect(width,height,10,10);
-            ctx.lineWidth=1;
-            ctx.shadowOffsetX=0;
-            ctx.shadowOffsetY=0;
-            // ctx.shadowBlur=25;
-          }
-
-          // console.log(d.data.length)
-
-          // console.log(d.width, d.height)
-          // console.log(message)
-          // console.log("rcv ms")
-        }
-        channel.onerror = () => {
-          console.log("channel error")
-        }
+        channel.onmessage = (event) => this.onChannelEvent(event);
       },
     )
   }
 
+  onChannelEvent(event) {
+    console.log("rcv")
+    let object;
+    try {
+      const encoded = decoder.decode(event.data);
+      object = JSON.parse(encoded);
+    } catch {
+      console.log("Error decoding data channel event")
+    }
+    console.log(object);
+  }
+
   render() {
-    this.setCanvasRef = element => {
-      this.canvas = element;
-    };
-
-
     return (
       <div className="App">
         <header className="App-header">
-        <p>Real-time SDK Demo</p>
-        <canvas ref={this.setCanvasRef} width={60} height={60} />
+        <p>Real-time SDK Template</p>
         </header>
       </div>
     );
   }
+}
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 export default App;
